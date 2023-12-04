717d5ad76c868effd87562524bb4660c69773051
Mohamed <hasanbouhlel@gmail.com>
2023-12-04 16:29:18 +0100

    made some small touches to backend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const cors = require("cors")
 const app = express()
+
+const PORT = 3000
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(PORT, () => {
+  console.log("Server Listening at http://localhost:3000")
+})
