de8256d8796afd0ec62b6668d07361dff21b72b8
Hyba <hibazoglami2018@gmail.com>
2023-12-10 21:45:40 +0100

    main fixed athor

--- my-project/src/SearchPage.jsx
+++ my-project/src/SearchPage.jsx
@@ -7,7 +7,6 @@ const SearchPage = ()=>{
 
     return (
         <>
-        
         <div className="relative w-full h-[1024px] overflow-hidden text-left text-3xl text-darkslategray font-estedad-vf">
         <div className="absolute top-[107px] left-[101px] w-[1284px] h-[177px]">
       <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-45xl font-rectaw01-mediumcondensed [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)]">
